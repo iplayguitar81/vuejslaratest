c8750b103db5a6de7624f370b7e4389e17213fc8
iplayguitar81 <iplayguitar81@yahoo.com>
2018-07-14 23:59:31 -0700

    testing push

--- app/Http/Controllers/BoxscoreController.php
+++ app/Http/Controllers/BoxscoreController.php
@@ -61,34 +61,47 @@ class BoxscoreController extends Controller
 //            ->where('team_json->city', 'Portland')
 //            ->get(['team_json']);
 
-        //$player_name = str_slug($slug);
 
        $player_name = str_replace('-', ' ', $slug);
        $player_name = str_replace('_', '-', $player_name);
 
 
-        $player = Boxscore::where([
-            ['boxscore_json->away_stats', 'like',  '%'.$player_name.'%'],
-            ['season_type', '=',  'regular'],
-        ])->orWhere([
-            ['boxscore_json->home_stats', 'like',  '%'.$player_name.'%'],
-            ['season_type', '=',  'regular'],
-        ])->groupBy('event_id')->get();
+       //this works case sensitive in JSON field:
 
+//        $player = Boxscore::where([
+//            ['boxscore_json->away_stats', 'like',  '%'.$player_name.'%'],
+//            ['season_type', '=',  'regular'],
+//        ])->orWhere([
+//            ['boxscore_json->home_stats', 'like',  '%'.$player_name.'%'],
+//            ['season_type', '=',  'regular'],
+//        ])->groupBy('event_id')->get();
 
 
        $player = Boxscore::whereRaw('lower(boxscore_json->"$.away_stats") like lower(?)', ["%{$player_name}%"])->where('season_type', '=', 'regular')->orWhereRaw('lower(boxscore_json->"$.home_stats") like lower(?)', ["%{$player_name}%"])->where('season_type', '=', 'regular')->groupBy('event_id')->get();
 
-
-
-//        $player = Boxscore::where(DB::raw('lower(boxscore_json->"$.away_stats")', 'like', DB::raw('lower(%{$player_name}%)'))
-//            ->where('season_type', '=', 'regular'))->get();
-
         $total_points = 0;
         $total_rebounds = 0;
+        $total_o_rebounds = 0;
+        $total_d_rebounds = 0;
+
         $total_blocks = 0;
         $total_steals = 0;
         $total_assists = 0;
+        $total_turnovers = 0;
+        $total_personal_fouls = 0;
+        $total_minutes = 0;
+        $field_goals_made = 0;
+        $field_goals_attempted = 0;
+        $free_throws_made = 0;
+        $free_throws_attempted = 0;
+
+        $three_point_field_goals_made  = 0;
+        $three_point_field_goals_attempted  = 0;
+        $starter_count = 0;
+
+
+        $display_name = '';
+
         $games_played = 0;
 
         foreach($player as $play)
@@ -96,14 +109,37 @@ class BoxscoreController extends Controller
 
             foreach($play['boxscore_json']['away_stats'] as $regis){
 
+            $display_name = $regis['display_name'];
 
             if(strtolower( $regis['display_name']) == $player_name){
 
                 $total_points+=$regis['points'];
+
                 $total_rebounds+=$regis['rebounds'];
+                $total_o_rebounds+=$regis['offensive_rebounds'];
+                $total_d_rebounds+=$regis['defensive_rebounds'];
+
                 $total_blocks+=$regis['blocks'];
                 $total_steals+=$regis['steals'];
                 $total_assists+=$regis['assists'];
+                $total_turnovers+=$regis['turnovers'];
+                $total_personal_fouls+=$regis['personal_fouls'];
+                $total_minutes+=$regis['minutes'];
+                $field_goals_made+=$regis['field_goals_made'];
+                $field_goals_attempted+=$regis['field_goals_attempted'];
+
+                $three_point_field_goals_made+=$regis['three_point_field_goals_made'];
+                $three_point_field_goals_attempted+=$regis['three_point_field_goals_attempted'];
+
+                $free_throws_made+=$regis['free_throws_made'];
+                $free_throws_attempted+=$regis['free_throws_attempted'];
+
+
+                if($regis['is_starter'] == 'true'){
+                    $starter_count+=1;
+                }
+
+
                 $games_played +=1;
 
             }
@@ -121,10 +157,28 @@ class BoxscoreController extends Controller
                 if(strtolower( $regis['display_name']) == $player_name){
 
                     $total_points+=$regis['points'];
+                    $total_o_rebounds+=$regis['offensive_rebounds'];
+                    $total_d_rebounds+=$regis['defensive_rebounds'];
                     $total_rebounds+=$regis['rebounds'];
                     $total_blocks+=$regis['blocks'];
                     $total_steals+=$regis['steals'];
                     $total_assists+=$regis['assists'];
+                    $total_turnovers+=$regis['turnovers'];
+                    $total_personal_fouls+=$regis['personal_fouls'];
+                    $total_minutes+=$regis['minutes'];
+                    $field_goals_made+=$regis['field_goals_made'];
+                    $field_goals_attempted+=$regis['field_goals_attempted'];
+                    $three_point_field_goals_made+=$regis['three_point_field_goals_made'];
+                    $three_point_field_goals_attempted+=$regis['three_point_field_goals_attempted'];
+                    $free_throws_made+=$regis['free_throws_made'];
+                    $free_throws_attempted+=$regis['free_throws_attempted'];
+
+
+                    if($regis['is_starter'] == 'true'){
+                        $starter_count+=1;
+                    }
+
+
                     $games_played +=1;
 
                 }
@@ -136,12 +190,44 @@ class BoxscoreController extends Controller
 
         if($games_played > 0) {
         $rs_points_avg = round(($total_points/$games_played),1);
+        $rs_mpg_avg = round(($total_minutes/$games_played),1);
+        $rs_to_avg = round(($total_turnovers/$games_played),1);
+        $rs_pf_avg = round(($total_personal_fouls/$games_played),1);
+        $rs_stl_avg = round(($total_steals/$games_played),1);
+        $rs_blk_avg = round(($total_blocks/$games_played),1);
+        $rs_ast_avg = round(($total_assists/$games_played),1);
+
+        $rs_reb_avg = round(($total_rebounds/$games_played),1);
+        $rs_o_reb_avg = round(($total_o_rebounds/$games_played),1);
+        $rs_d_reb_avg = round(($total_d_rebounds/$games_played),1);
+        $rs_fgm_avg = round(($field_goals_made/$games_played),1);
+        $rs_fga_avg = round(($field_goals_attempted/$games_played),1);
+        $rs_fg_pct_avg = round(($field_goals_made/$field_goals_attempted),3);
+
+        $rs_3ptm_avg = round(($three_point_field_goals_made/$games_played),1);
+        $rs_3pta_avg = round(($three_point_field_goals_attempted/$games_played),1);
+        $rs_3pt_pct_avg = round(($three_point_field_goals_made/$three_point_field_goals_attempted),3);
+
+        $rs_ftm_avg = round(($free_throws_made/$games_played),1);
+        $rs_fta_avg = round(($free_throws_attempted/$games_played),1);
+        $rs_ft_pct_avg = round(($free_throws_made/$free_throws_attempted),3);
+
+
+
+
+
+
+
+
+
         }
         else {
             $rs_points_avg = 0;
         }
 
-        return view('boxscore.show-stats')->with(compact('team','player','total_points', 'total_rebounds', 'total_blocks', 'total_steals', 'total_assists', 'games_played', 'rs_points_avg', 'player_name'));;
+
+
+        return view('boxscore.show-stats')->with(compact('team','player','total_points', 'total_rebounds', 'total_o_rebounds', 'total_d_rebounds', 'total_blocks', 'total_steals', 'total_assists', 'games_played', 'rs_points_avg', 'player_name', 'total_turnovers', 'total_personal_fouls', 'total_minutes', 'field_goals_made', 'field_goals_attempted', 'three_point_field_goals_made', 'three_point_field_goals_attempted', 'free_throws_made', 'free_throws_attempted', 'rs_mpg_avg', 'rs_to_avg', 'rs_pf_avg', 'rs_stl_avg', 'rs_blk_avg', 'rs_ast_avg', 'rs_reb_avg', 'rs_o_reb_avg', 'rs_d_reb_avg', 'rs_fgm_avg', 'rs_fga_avg','rs_fg_pct_avg', 'rs_3ptm_avg', 'rs_3pta_avg', 'rs_3pt_pct_avg', 'rs_ftm_avg', 'rs_fta_avg', 'rs_ft_pct_avg', 'starter_count'));;
 
     }
 
--- resources/views/boxscore/show-stats.blade.php
+++ resources/views/boxscore/show-stats.blade.php
@@ -1,3 +1,7 @@
+@extends('layout')
+@section('title', $player_name.' Statistics')
+@section('content')
+
 
 @if($games_played == 0)
 
@@ -5,28 +9,80 @@
 
     @else
 
-<p>{{$player_name}} Regular Season Stats:</p>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h2 class="display-5 text-center">
+                {{$player_name}}
+                <p>Regular Season Statistics</p>
+            </h2>
 
-<hr/>
-
-<p>Total Points:  {{$total_points}}</p>
-
-<p>Points Per Game:  {{$rs_points_avg}}</p>
-
-<p>Total Rebounds:  {{$total_rebounds}}</p>
-
-<p>Total Blocks:  {{$total_blocks}}</p>
-
-<p>Total Steals:  {{$total_steals}}</p>
-
-<p>Total Assists:  {{$total_assists}}</p>
+        </div>
+    </div>
 
 
-<p>Total Regular Season Games Played:  {{$games_played}}</p>
+
+    <div class="container">
 
 
+
+
+        <h3>Regular Season Averages</h3>
+        <table class="table table-sm" id="last_game_period_box">
+            <tbody><tr><th>GP</th><th>GS</th><th>MIN</th><th>FGM-A</th><th>FG%</th><th>3PM-A</th><th>3PT%</th><th>FTM-A</th><th>FT%</th><th>OR</th><th>DR</th><th>REB</th><th>AST</th><th>BLK</th><th>STL</th><th>PF</th><th>TO</th><th>PTS</th></tr>
+            <tr><td>{{$games_played}}</td><td>{{$starter_count}}</td><td>{{$rs_mpg_avg}}</td><td>{{$rs_fgm_avg}}-{{$rs_fga_avg}}</td><td>{{$rs_fg_pct_avg}}</td><td>{{$rs_3ptm_avg}}-{{$rs_3pta_avg}}</td><td>{{$rs_3pt_pct_avg}}</td><td>{{$rs_ftm_avg}}-{{$rs_fta_avg}}</td><td>{{$rs_ft_pct_avg}}</td><td>{{$rs_o_reb_avg}}</td><td>{{$rs_d_reb_avg}}</td><td>{{$rs_reb_avg}}</td><td>{{$rs_ast_avg}}</td><td>{{$rs_blk_avg}}</td><td>{{$rs_stl_avg}}</td><td>{{$rs_pf_avg}}</td><td>{{$rs_to_avg}}</td><td>{{$rs_points_avg}}</td></tr>
+            </tbody>
+        </table>
+
+
+        <br/>
+        <br/>
+        <h3>Regular Season Totals</h3>
+        <table class="table table-sm" id="last_game_period_box">
+            <tbody><tr><th>FGM-A</th><th>FG%</th><th>3PM-A</th><th>3PT%</th><th>FTM-A</th><th>FT%</th><th>OR</th><th>DR</th><th>REB</th><th>AST</th><th>BLK</th><th>STL</th><th>PF</th><th>TO</th><th>PTS</th></tr>
+            <tr><td>{{$field_goals_made}}-{{$field_goals_attempted}}</td><td>{{$rs_fg_pct_avg}}</td><td>{{$three_point_field_goals_made}}-{{$three_point_field_goals_attempted}}</td><td>{{$rs_3pt_pct_avg}}</td><td>{{$free_throws_made}}-{{$free_throws_attempted}}</td><td>{{$rs_ft_pct_avg}}</td><td>{{$total_o_rebounds}}</td><td>{{$total_d_rebounds}}</td><td>{{$total_rebounds}}</td><td>{{$total_assists}}</td><td>{{$total_blocks}}</td><td>{{$total_steals}}</td><td>{{$total_personal_fouls}}</td><td>{{$total_turnovers}}</td><td>{{$total_points}}</td></tr>
+            </tbody>
+        </table>
+
+
+    </div>
+
+        {{--<p>Total Points:  {{$total_points}}</p>--}}
+
+{{--<p>Points Per Game:  {{$rs_points_avg}}</p>--}}
+
+{{--<p>Total Rebounds:  {{$total_rebounds}}</p>--}}
+
+{{--<p>Total Blocks:  {{$total_blocks}}</p>--}}
+
+{{--<p>Total Steals:  {{$total_steals}}</p>--}}
+
+{{--<p>Total Assists:  {{$total_assists}}</p>--}}
+
+{{--<p>Total Turnovers:  {{$total_turnovers}}</p>--}}
+
+{{--<p>Total Personal Fouls:  {{$total_personal_fouls}}</p>--}}
+
+
+    {{--<p>Total Minutes:  {{$total_minutes}}</p>--}}
+
+
+
+
+
+
+
+
+{{--<p>Total Regular Season Games Played:  {{$games_played}}</p>--}}
+
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
 
     @endif
 
 
 
+@endsection

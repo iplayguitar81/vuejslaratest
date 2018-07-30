fa421e2e7d4979133d30e1363de43303c59c3456
iplayguitar81 <iplayguitar81@yahoo.com>
2018-07-29 23:18:41 -0700

    testing push

--- resources/views/schedule/show.blade.php
+++ resources/views/schedule/show.blade.php
@@ -9,8 +9,20 @@
 
     <p>{{ gameDate($schedule->event_date)  }} </p>
 
-    <p><a href="{{url('/boxscores/'.$schedule->event_id)}}"> {{$schedule->schedule_json['team']['full_name'].' '. $schedule->schedule_json['team_points_scored']  .' @ '. $schedule->schedule_json['opponent']['full_name'].' '. $schedule->schedule_json['opponent_points_scored'].' '.$schedule->schedule_json['team_event_result'] }} </a></p>
+@if($schedule->schedule_json['team_event_location_type'] == 'h')
 
+    <p><a href=" {{url('/boxscores/'.$schedule->event_id)}}"> {{$schedule->schedule_json['opponent']['full_name'].' '. $schedule->schedule_json['opponent_points_scored'].' @ '.$schedule->schedule_json['team']['full_name'].' '. $schedule->schedule_json['team_points_scored']}}</a> </p>
+
+    @else
+
+    <p><a href="{{url('/boxscores/'.$schedule->event_id)}}"> {{$schedule->schedule_json['team']['full_name'].' '. $schedule->schedule_json['team_points_scored']  .' @ '. $schedule->schedule_json['opponent']['full_name'].' '. $schedule->schedule_json['opponent_points_scored']}} </a></p>
+
+
+    @endif
+
+
+
+        <p><b>{{$team->team_json['last_name'].' '. $schedule->schedule_json['team_event_result']}}</b></p>
 
     <hr/>
 

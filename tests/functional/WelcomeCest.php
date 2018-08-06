8d9c0cb7025a837a452bd0abc3727d3fee8c301b
iplayguitar81 <iplayguitar81@yahoo.com>
2018-08-05 22:40:21 -0700

    testing push

---
+++ tests/functional/WelcomeCest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class WelcomeCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    public function _after(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('LARACASTS');
+
+    }
+}
